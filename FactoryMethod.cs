8c6603715751aec6ada41089321cbc806435152b
DenisIosip <62891947+DenisIosip@users.noreply.github.com>
2022-05-12 14:21:53 +0300

    Add files via upload

---
+++ FactoryMethod.cs
@@ -0,0 +1,110 @@
+﻿namespace RefactoringGuru.DesignPatterns.FactoryMethod.Conceptual
+{
+    // Clasa Creator declară o metodă din fabrică care trebuie să revină
+    // obiect de clasă Product. Subclasele de creatori oferă de obicei
+    // implementarea acestei metode.
+    abstract class Creator
+    {
+        // Rețineți că Creatorul poate oferi și o implementare
+        // metoda implicită din fabrică.
+        public abstract IProduct FactoryMethod();
+
+        // De asemenea, rețineți că, în ciuda numelui, responsabilitatea principală
+        // Creator nu este despre crearea de produse. De obicei conține
+        // o logică de bază de afaceri care se bazează pe obiecte
+        // Produse returnate prin metoda fabricii. Subclasele pot indirect
+        // modifică această logică de afaceri, suprascriind metoda din fabrică și revenind
+        // alt tip de produs din acesta.
+        public string SomeOperation()
+        {
+            // Apelați metoda din fabrică pentru a obține obiectul produs.
+            var product = FactoryMethod();
+            // Apoi, lucrați cu acest produs.
+            var result = "Creator: The same creator's code has just worked with "
+                + product.Operation();
+
+            return result;
+        }
+    }
+
+    // Creatorii  înlocuiesc metoda din fabrică pentru a
+    // schimba tipul produsului rezultat.
+    class ConcreteCreator1 : Creator
+    {
+        // Rețineți că semnătura metodei folosește în continuare tipul
+        // produs abstract, deși returnat de fapt din metodă
+        // produs specific. Deci Creatorul poate rămâne
+        // independent de clasele specifice de produse.
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+
+    class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
+
+    // Interfața de produs declară operațiunile pe care toate
+    // produse specifice.
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    // Produse specifice oferă diferite implementări de interfață
+    // Produs.
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct1}";
+        }
+    }
+
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct2}";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ConcreteCreator2.");
+            ClientCode(new ConcreteCreator2());
+        }
+
+        // Codul client funcționează cu o instanță a unui anumit creator, deși
+        // prin interfața de bază. În timp ce clientul continuă să lucreze cu
+        // de către creator prin interfața de bază, puteți trece oricare
+        // subclasa creator.
+        public void ClientCode(Creator creator)
+        {
+            // ...
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+            // ...
+        }
+    }
+
+    class FactoryMethod
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+        }
+    }
+}
---
+++ SingletonMethod.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
+{
+    // Clasa Singleton oferă o metodă `GetInstance` care se comportă ca
+    // constructor alternativ și permite clienților să obțină același lucru
+    // o instanță a clasei la fiecare apel.
+
+    // Singleton-ul ar trebui să fie întotdeauna o clasă „sigilată” pentru a preveni clasa
+    // moștenire prin clase externe și, de asemenea, prin clase imbricate.
+    public sealed class Singleton
+    {
+        // Constructorul Singleton ar trebui să fie întotdeauna ascuns pentru a preveni
+        // creează un obiect prin operatorul nou.
+        private Singleton() { }
+
+        // Obiectul singleton este stocat într-un câmp de clasă static. Exista
+        // mai multe moduri de a inițializa acest câmp și toate au diferite
+        // avantaje și dezavantaje. În acest exemplu, vom lua în considerare cel mai simplu dintre
+        // le, al căror dezavantaj este incapacitatea completă de a corect
+        // lucrează într-un mediu cu mai multe fire.
+        private static Singleton _instance;
+
+        // Aceasta este o metodă statică care controlează accesul la instanța singleton.
+        // La prima rulare, instanțiază un singleton și îl plasează
+        // câmp static. La rulările ulterioare, se întoarce la client
+        // obiect stocat într-un câmp static.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // În sfârșit, orice singleton ar trebui să conțină o logică de afaceri,
+        // care poate fi executat pe instanța sa.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cod client.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
